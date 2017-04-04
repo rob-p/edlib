b0070b6bc5763eccf35fc52e775802870520db3b
Rob Patro <rob.patro@cs.stonybrook.edu>
2017-04-04 16:57:10 -0400

    very hacky first approach at AlignerEngine

--- test/runTests.cpp
+++ test/runTests.cpp
@@ -10,6 +10,8 @@ using namespace std;
 
 
 bool runRandomTests(int numTests, EdlibAlignMode mode, bool findAlignment);
+bool runRandomTestsAlignerEngine(int numTests, EdlibAlignMode mode, bool findAlignment);
+
 bool runTests();
 
 int calcEditDistanceSimple(const char* query, int queryLength,
@@ -40,6 +42,7 @@ int main(int argc, char* argv[]) {
     srand(42);
     bool allTestsPassed = true;
 
+    /*
     printf("Testing HW with alignment...\n");
     allTestsPassed &= runRandomTests(numRandomTests, EDLIB_MODE_HW, true);
     printf("\n");
@@ -51,11 +54,21 @@ int main(int argc, char* argv[]) {
     printf("Testing NW with alignment...\n");
     allTestsPassed &= runRandomTests(numRandomTests, EDLIB_MODE_NW, true);
     printf("\n");
+    */
 
     printf("Testing NW...\n");
     allTestsPassed &= runRandomTests(numRandomTests, EDLIB_MODE_NW, false);
     printf("\n");
 
+    printf("Testing NW (AlignerEngine)...\n");
+    allTestsPassed &= runRandomTestsAlignerEngine(numRandomTests, EDLIB_MODE_NW, false);
+    printf("\n");
+
+    printf("Testing NW (2)...\n");
+    allTestsPassed &= runRandomTests(numRandomTests, EDLIB_MODE_NW, false);
+    printf("\n");
+
+    /*
     printf("Testing SHW with alignment...\n");
     allTestsPassed &= runRandomTests(numRandomTests, EDLIB_MODE_SHW, true);
     printf("\n");
@@ -63,6 +76,7 @@ int main(int argc, char* argv[]) {
     printf("Testing SHW...\n");
     allTestsPassed &= runRandomTests(numRandomTests, EDLIB_MODE_SHW, false);
     printf("\n");
+    */
 
     printf("Specific tests:\n");
     bool specificTestsPassed = runTests();
@@ -76,14 +90,151 @@ int main(int argc, char* argv[]) {
 }
 
 
-void fillRandomly(char* seq, int seqLength, int alphabetLength) {
+void fillRandomlyDNA(char* seq, int seqLength, int alphabetLength) {
+  char chars[] = {'A', 'C', 'G', 'G'};
     for (int i = 0; i < seqLength; i++)
-        seq[i] = rand() % alphabetLength;
+      seq[i] = chars[rand() % alphabetLength];
+}
+
+void fillRandomly(char* seq, int seqLength, int alphabetLength) {
+  for (int i = 0; i < seqLength; i++)
+    seq[i] = rand() % alphabetLength;
+}
+
+bool runRandomTestsAlignerEngine(int numTests, EdlibAlignMode mode, bool findAlignment) {
+    int alphabetLength = 4;
+    int numTestsFailed = 0;
+    clock_t start;
+    double timeEdlib = 0;
+    double timeSimple = 0;
+
+    AlignerEngine ae;
+
+    for (int i = 0; i < numTests; i++) {
+        bool failed = false;
+        int queryLength = 100 + rand() % 20;
+        int targetLength = 100 + rand() % 40;
+        char* query = (char *) malloc(sizeof(char) * queryLength);
+        char* target = (char *) malloc(sizeof(char) * targetLength);
+        fillRandomlyDNA(query, queryLength, alphabetLength);
+        fillRandomlyDNA(target, targetLength, alphabetLength);
+
+        // // Print query
+        // printf("Query: ");
+        // for (int i = 0; i < queryLength; i++)
+        //     printf("%d", query[i]);
+        // printf("\n");
+
+        // // Print target
+        // printf("Target: ");
+        // for (int i = 0; i < targetLength; i++)
+        //     printf("%d", target[i]);
+        // printf("\n");
+
+        start = clock();
+        ae(query, queryLength, target, targetLength,
+           edlibNewAlignConfig(-1, mode, findAlignment ? EDLIB_TASK_PATH : EDLIB_TASK_DISTANCE));
+        auto& result = ae.result();
+        timeEdlib += clock() - start;
+        if (findAlignment){//}result.alignment) {
+            if (!checkAlignment(query, queryLength, target, targetLength,
+                                result.editDistance, result.endLocations[0], mode,
+                                const_cast<unsigned char*>(result.alignment.data()), result.alignmentLength)) {
+                failed = true;
+                printf("Alignment is not correct\n");
+            }
+            int alignmentStart = getAlignmentStart(const_cast<unsigned char*>(result.alignment.data()), result.alignmentLength,
+                                                   result.endLocations[0]);
+            if (result.startLocations[0] != alignmentStart) {
+                failed = true;
+                printf("Start location (%d) is not consistent with alignment start (%d)\n",
+                       result.startLocations[0], alignmentStart);
+            }
+        }
+
+        start = clock();
+        int score2; int numLocations2;
+        int* endLocations2;
+        calcEditDistanceSimple(query, queryLength, target, targetLength,
+                               mode, &score2, &endLocations2, &numLocations2);
+        timeSimple += clock() - start;
+
+        // Compare results
+        if (result.editDistance != score2) {
+            failed = true;
+            printf("Scores are different! Expected %d, got %d)\n", score2, result.editDistance);
+        } else if (result.editDistance == -1 && !(result.endLocations.size() == 0)) {
+            failed = true;
+            printf("Score was not found but endLocations is not NULL!\n");
+        } else if (result.numLocations != numLocations2) {
+            failed = true;
+            printf("Number of endLocations returned is not equal! Expected %d, got %d\n",
+                   numLocations2, result.numLocations);
+        } else {
+            for (int i = 0; i < result.numLocations; i++) {
+                if (result.endLocations[i] != endLocations2[i]) {
+                    failed = true;
+                    printf("EndLocations at %d are not equal! Expected %d, got %d\n",
+                           i, endLocations2[i], result.endLocations[i]);
+                    break;
+                }
+            }
+        }
+
+        //edlibFreeAlignResult(result);
+        if (endLocations2) delete [] endLocations2;
+
+        for (int k = max(score2 - 1, 0); k <= score2 + 1; k++) {
+            int scoreExpected = score2 > k ? -1 : score2;
+            EdlibAlignResult result3 = edlibAlign(
+                    query, queryLength, target, targetLength,
+                    edlibNewAlignConfig(k, mode, findAlignment ? EDLIB_TASK_PATH : EDLIB_TASK_DISTANCE));
+            if (result3.editDistance != scoreExpected) {
+                failed = true;
+                printf("For k = %d score was %d but it should have been %d\n",
+                       k, result3.editDistance, scoreExpected);
+            }
+            if (result3.alignment) {
+                if (!checkAlignment(query, queryLength, target, targetLength,
+                                    result3.editDistance, result3.endLocations[0],
+                                    mode, result3.alignment, result3.alignmentLength)) {
+                    failed = true;
+                    printf("Alignment is not correct\n");
+                }
+                int alignmentStart = getAlignmentStart(result3.alignment, result3.alignmentLength,
+                                                       result3.endLocations[0]);
+                if (result3.startLocations[0] != alignmentStart) {
+                    failed = true;
+                    printf("Start location (%d) is not consistent with alignment start (%d)\n",
+                           result3.startLocations[0], alignmentStart);
+                }
+            }
+            edlibFreeAlignResult(result3);
+        }
+
+        if (failed)
+            numTestsFailed++;
+
+        free(query);
+        free(target);
+    }
+
+    printf(mode == EDLIB_MODE_HW ? "HW: " : mode == EDLIB_MODE_SHW ? "SHW: " : "NW: ");
+    printf(numTestsFailed == 0 ? "\x1B[32m" : "\x1B[31m");
+    printf("%d/%d", numTests - numTestsFailed, numTests);
+    printf("\x1B[0m");
+    printf(" random tests passed!\n");
+    double mTime = ((double)(timeEdlib))/CLOCKS_PER_SEC;
+    double sTime = ((double)(timeSimple))/CLOCKS_PER_SEC;
+    printf("Time Edlib: %lf\n", mTime);
+    printf("Time Simple: %lf\n", sTime);
+    printf("Times faster: %.2lf\n", sTime / mTime);
+    return numTestsFailed == 0;
 }
 
 // Returns true if all tests passed, false otherwise.
 bool runRandomTests(int numTests, EdlibAlignMode mode, bool findAlignment) {
-    int alphabetLength = 10;
+    int alphabetLength = 4;
     int numTestsFailed = 0;
     clock_t start;
     double timeEdlib = 0;
@@ -91,12 +242,12 @@ bool runRandomTests(int numTests, EdlibAlignMode mode, bool findAlignment) {
 
     for (int i = 0; i < numTests; i++) {
         bool failed = false;
-        int queryLength = 50 + rand() % 300;
-        int targetLength = 500 + rand() % 10000;
+        int queryLength = 100 + rand() % 20;
+        int targetLength = 100 + rand() % 40;
         char* query = (char *) malloc(sizeof(char) * queryLength);
         char* target = (char *) malloc(sizeof(char) * targetLength);
-        fillRandomly(query, queryLength, alphabetLength);
-        fillRandomly(target, targetLength, alphabetLength);
+        fillRandomlyDNA(query, queryLength, alphabetLength);
+        fillRandomlyDNA(target, targetLength, alphabetLength);
 
         // // Print query
         // printf("Query: ");
